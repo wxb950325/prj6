fb056ff96830a144d4c18d0a675dbcf75465766d
byb <1156387092@qq.com>
2019-08-21 15:39:46 +0800

    添加了mapperscan和servletComponentscan

--- src/main/java/com/woniu/Prj6Application.java
+++ src/main/java/com/woniu/Prj6Application.java
@@ -1,9 +1,13 @@
 package com.woniu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.woniu.mapper")
+@ServletComponentScan("com.woniu.filter")
 public class Prj6Application {
 
 	public static void main(String[] args) {
